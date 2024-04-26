6083413bffb3c8a537c15017f916247725c8a44c
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2024-04-26 02:00:53 +0100

    Create constants.rs

---
+++ src/testing/constants.rs
@@ -0,0 +1,20 @@
+#![allow(dead_code)]
+pub const ADMIN: &str = "admin";
+
+pub const ADMIN2: &str = "admin2";
+
+pub const CREATOR: &str = "creator";
+
+pub const SHOPPER: &str = "shopper";
+
+pub const UNAUTHORIZED: &str = "unauthorized";
+
+pub const NATIVE_DENOM: &str = "uxion";
+
+pub const MOCK_MINT_FEE: u128 = 1000000;
+
+pub const MOCK_MINT_PERCENT: u128 = 2;
+
+pub const MOCK_COLLECTION_NAME: &str = "product";
+
+pub const MOCK_COLLECTION_SYMBOL: &str = "PROD";
