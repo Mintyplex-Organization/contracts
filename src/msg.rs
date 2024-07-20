cc307a8584b3927fc2b7adedd99557901b6e3e3f
Perelyn <okohebina@gmail.com>
2024-07-20 13:42:45 +0100

    add query responses

--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,6 @@
-use crate::state;
+use crate::state::{self, CollectionInfo};
 use crate::state::{Config, UpdateMintFeeParams, WithdrawParams};
-use cosmwasm_schema::cw_serde;
+use cosmwasm_schema::{cw_serde, QueryResponses};
 use cosmwasm_std::Addr;
 use state::{CollectionParams, MintParams};
 
@@ -20,17 +20,55 @@ pub enum ExecuteMsg {
 }
 
 #[cw_serde]
-// #[derive(QueryResponses)]
+#[derive(QueryResponses)]
 pub enum QueryMsg {
     // Query for config
+    #[returns(ConfigResponse)]
     Config {},
 
     // Query for the current token index
+    #[returns(u64)]
     TokenIndex {},
 
     // Query for collections created by a specific creator
+    #[returns(CollectionInfoResponse)]
     CreatorCollections {
         creator: Addr,
         collection_name: String,
     },
+}
+
+// We define a custom struct for each query response
+#[cw_serde]
+pub struct CollectionInfoResponse {
+    pub name: String,
+    pub symbol: String,
+    pub mint_fee: u128,
+    pub collection_address: Option<Addr>,
+}
+
+impl From<CollectionInfo> for CollectionInfoResponse {
+    fn from(collection_info: CollectionInfo) -> CollectionInfoResponse {
+        CollectionInfoResponse {
+            name: collection_info.name,
+            symbol: collection_info.symbol,
+            mint_fee: collection_info.mint_fee,
+            collection_address: collection_info.collection_address,
+        }
+    }
+}
+
+#[cw_serde]
+pub struct ConfigResponse {
+    pub owner: Addr,
+    pub mint_percent: u128,
+}
+
+impl From<Config> for ConfigResponse {
+    fn from(config: Config) -> ConfigResponse {
+        ConfigResponse {
+            owner: config.owner,
+            mint_percent: config.mint_percent,
+        }
+    }
 }
